450f9b66b9ce8e349cea8938a22b062f91d25b21
jonathan <jonathangalimberti@hotmail.com>
2026-06-03 20:19:54 -0300

    solucion de fata de renderizacion del elemento Item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,23 +2,26 @@ import "./item.css"
 import react from "react";
 
 function Item ({pokemon}) {
-  console.log("Esto es pokemon en Item ", pokemon);
-  react.useEffect(()=>{
-    console.log("Esto es pokemon en Item con useEffect ", pokemon);
-  }, [pokemon])
+  
+  if (!pokemon.length) {
+    return <p>Loading...</p>;
+  }
 
-  pokemon.forEach(pokemon => {
-    if(pokemon === undefined){
-      return (
-              <div className="container">
-                <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                <p>{pokemon.name}</p>
-                <p >{pokemon.id}</p>
-              </div>);
-      }else {
-      return <p>Loading...</p>
-      }
-  })
+  return (
+    <>
+      {pokemon.map((poke) => (
+        <div key={poke.id} className="container">
+          <img
+            src={poke.sprites.front_default}
+            alt={poke.name}
+          />
+          <p>{poke.name}</p>
+          <p>{poke.id}</p>
+        </div>
+      ))}
+    </>
+  );
+  
    
 }
 
